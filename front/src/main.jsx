dfcd5d13780f1c7ffa028ac2d510640ba7dfd34d
Jackson <wnj9tx@virginia.edu>
2025-05-27 11:54:07 -0400

    sidebar

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import Forum from './routes/Forum.jsx'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
+  { path: '/forum', element: <Forum />}
 ]);
 
 createRoot(document.getElementById('root')).render(
