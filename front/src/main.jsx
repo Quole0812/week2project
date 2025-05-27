2e53f3cd36f94b0a3550634217553a360e1a7f74
BenjaminVC <ben@benvancise.com>
2025-05-27 08:42:04 -0700

    modified Discover components, added route to get all users in back/routes/profile.js

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import Discover from './routes/Discover.jsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
+  { path: '/discover', element: <Discover /> }
 ]);
 
 createRoot(document.getElementById('root')).render(
---
+++ front/src/routes/Discover.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import DiscoverGrid from "../components/DiscoverGrid";
+
+export default function Discover() {
+
+    return (
+        <>
+            <DiscoverGrid />
+        </>
+    );
+}
