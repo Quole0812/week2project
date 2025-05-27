2364f90e82807835991218cdda08e768450e6dbf
Ben <46990784+BenjaminVC@users.noreply.github.com>
2025-05-27 11:24:45 -0700

    Update main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,22 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AuthProvider } from "./components/AuthContext"
 import App from './App.jsx'
-<<<<<<< HEAD
+
 import Forum from './routes/Forum.jsx'
 import Profile from './routes/Profile.jsx'
+import Discover from './routes/Discover.jsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/profile/:id', element: <Profile /> },
   { path: '/forum', element: <Forum />},
-
-=======
-import Discover from './routes/Discover.jsx';
-
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
   { path: '/discover', element: <Discover /> }
->>>>>>> discover
 ]);
 
 createRoot(document.getElementById('root')).render(
