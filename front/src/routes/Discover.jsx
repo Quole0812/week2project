66076668609a96c7b66f8ab7885568cf54ea9231
BenjaminVC <ben@benvancise.com>
2025-05-27 20:05:07 -0700

    updated css

--- front/src/routes/Discover.jsx
+++ front/src/routes/Discover.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect, use } from "react";
+import { AuthContext } from "../components/AuthContext";
+import Sidebar from "../components/Sidebar/Sidebar";
 import DiscoverGrid from "../components/Discover/DiscoverGrid";
 import axios from "axios";
 
@@ -7,6 +9,7 @@ export default function Discover() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    
 
     const apiUrl = "http://127.0.0.1:3001/profile";
       
@@ -28,28 +31,39 @@ export default function Discover() {
     }, [])
     
     return (
-        <>
-            {loading ? (
-                <div>Loading...</div>
-            ) :
-            error ? (
-                <div>Error: {error}</div>
-            ) : (
-                <div>
-                    <h1>Discover Users</h1>
-                    {users.map((user => (
-                        <div key={user.id}>
-                            {/* pass these names to DiscoverGrid */}
-                            <strong>{user.name}</strong>  
-                            <br />
-                        </div>
-                        
-                        
-                    )
-                    ))}
+            <div className="discover-main-bg">
+                <h1 className="discover-results-header">Discover</h1>
+                <div className="discover-main-content">
                     
+                    {loading ? (
+                    <div>Loading...</div>
+                    ) :
+                    error ? (
+                        <div>Error: {error}</div>
+                    ) : (
+                            <div className="discover-container">
+                                
+                                {users.map((user => (
+                                    <div className="discover-card" key={user.id}>
+                                        {/* pass these names to DiscoverGrid */}
+                                        <strong className="discover-card-text"><span style={{ verticalAlign: 'bottom' }}>{user.name}</span></strong>  
+                                        {/* <br /> */}
+                                    </div>
+                                    
+                                
+                                
+                            )
+                            ))}
+                            <div className="discover-card" >
+                                    test
+                                    </div>
+                            </div>
+                    )}
                 </div>
-            )}
-        </>
+                {/* <Sidebar /> */}
+                
+                
+                
+            </div>
     );
 }
